68e7166cd142761dd986dc3053069276410a0952
Vernon <baldeojohnvernonbalaorp@gmail.com>
2025-02-24 19:57:22 +0800

    minor edit on display

--- index.php
+++ index.php
@@ -959,7 +959,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <?php endif; ?>
 
         <center>
-            <img src="images\Upgrade.png" alt="Upgrade" style="margin-top: 20px;">
+            <img src="images/Upgrade.png" alt="Upgrade" style="margin-top: 20px; max-width: 100%; height: auto; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); border-radius: 8px;">
         </center>
 
     </body>
